8593495c6a7bae14140440aeadbbc6c8f9bc237d
Sergiy@sergi <sergiyzabach512@gmail.com>
2025-04-06 17:14:16 +0300

    bit changes 2

--- LW2/LW2.cpp
+++ LW2/LW2.cpp
@@ -55,7 +55,7 @@ void outputData(double start, double end, double step, int n)
         secondArr[i][1] = y(x, n);
         x += step;
     }
-    ofstream outputFile("first_out.txt");
+    ofstream outputFile("output.txt");
     if (!outputFile) {
         cerr << "Error opening file for writing!" << endl;
         return;
@@ -65,7 +65,7 @@ void outputData(double start, double end, double step, int n)
         outputFile << secondArr[i][0] << "\t" << secondArr[i][1] << endl;
     }
     outputFile.close();
-    cout << "Data has been written to first_out.txt" << endl;
+    cout << "Data has been written to output.txt" << endl;
     for (int i = 0; i < rows; i++) {
         delete[] secondArr[i];
     }
