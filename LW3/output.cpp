87029460ea2d74e9880f5fc6915c165d59e67fa1
Sergiy@sergi <sergiyzabach512@gmail.com>
2025-05-03 14:35:16 +0300

    Added files for LW3

--- LW3/output.cpp
+++ LW3/output.cpp
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-void outputTable(double start, double end, double step, int n, MathFunc func) {
+void outputTable(double start, double end, double step, int n, MathFunc fun) {
     cout << "x \ty(x)" << endl;
 
     for (double x = start; x <= end; x += step) {
-        double y = func(x, n);
+        double y = fun(x, n);
 
         cout << x << "\t";
-        if (y == -1 && func == FunctionSqrtX) {
+        if (y == -1 && fun == FunctionSqrtX) {
             cout << "y is not defined" << endl;
         }
         else {
--- LW3/output.h
+++ LW3/output.h
@@ -3,7 +3,7 @@
 #define FUNCTION_TABLE_H
 
 typedef double (*MathFunc)(double, int);
-double FunctionSqrtX(double x, int n); // porivnyanna v outputtable.cpp
-void outputTable(double start, double end, double step, int n, MathFunc func);
+double FunctionSqrtX(double x, int n); // porivnyanna v output.cpp
+void outputTable(double start, double end, double step, int n, MathFunc fun);
 
 #endif
