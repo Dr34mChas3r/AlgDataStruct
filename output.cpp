95b3d118a7f89941c5b1019827be6445bb02e460
Sergiy@sergi <sergiyzabach512@gmail.com>
2025-05-03 14:54:58 +0300

    Revert "Commit"
    This reverts commit c6410bb4afcc541bb93b8bfcdda26b41fd430db2.

---
+++ output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "output.h"
+
+using namespace std;
+
+void outputTable(double start, double end, double step, int n, MathFunc func) {
+    cout << "x \ty(x)" << endl;
+
+    for (double x = start; x <= end; x += step) {
+        double y = func(x, n);
+
+        cout << x << "\t";
+        if (y == -1 && func == FunctionSqrtX) {
+            cout << "y is not defined" << endl;
+        }
+        else {
+            cout << y << endl;
+        }
+    }
+}
