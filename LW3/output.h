890df1e4d8099312c227f44bb5a049ef09af2a49
Dr34mChas3r <53311619+Dr34mChas3r@users.noreply.github.com>
2025-05-22 12:20:01 +0300

    Update output.h

--- LW3/output.h
+++ LW3/output.h
@@ -3,7 +3,7 @@
 #define FUNCTION_TABLE_H
 
 typedef double (*MathFunction)(double, int);
-double squareRoot(double x, int n); // Dlya porivnyannya v file output.cpp ( if x<0, y=not defined )
+double squareRoot(double x, int n); 
 void PrintFunctionTable(double start, double end, double step, int n, MathFunction func);
 
 #endif
